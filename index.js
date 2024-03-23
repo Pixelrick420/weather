a998d10c8851dc53f367dbbace1a0c5df848207c
Harikrishnan R <106871878+Pixelrick420@users.noreply.github.com>
2024-03-23 20:22:42 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,34 @@
+var search = document.getElementById('search');
+var input = document.getElementById('input');
+var para = document.getElementById('result');
+var lochead = document.getElementById('lochead');
+search.addEventListener('click', getRegion);
+
+function getRegion() {
+    var location = input.value.trim(); // Trim whitespace from input
+    if (location === '') {
+        para.innerText = "Please enter a location.";
+        return;
+    }
+
+    var api_link = `http://api.weatherapi.com/v1/current.json?key=c74290ef12984eb39a2112628240703&q=${location}&aqi=no`;
+    
+    fetch(api_link)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            lochead.innerText = "Weather in " + location;
+            para.innerText = 
+                "Temperature : " + data.current.temp_c + "°C" +
+                "\n Condition : " + data.current.condition.text +
+                "\n Humidity : " + data.current.humidity + " percent" +
+                "\n Wind : " + data.current.wind_kph + " km/h";
+        })
+        .catch(error => {
+            para.innerText = "Could not find weather data";
+        });
+}
