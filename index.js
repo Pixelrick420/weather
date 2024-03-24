2e11babd5568a446e4eab7b9ea61993fc37cf164
Harikrishnan R <106871878+Pixelrick420@users.noreply.github.com>
2024-03-24 15:10:08 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -11,26 +11,30 @@ function getRegion() {
         para.innerText = "Please enter a location.";
         return;
     }
-
-    var api_link = `http://api.weatherapi.com/v1/current.json?key=c74290ef12984eb39a2112628240703&q=${location}&aqi=no`;
+    else if(location.toUpperCase() === "thanos"){
+        para.innerText = "easter egg in deevelopment";
+    }
+    else{
+        var api_link = `http://api.weatherapi.com/v1/current.json?key=c74290ef12984eb39a2112628240703&q=${location}&aqi=no`;
     
-    fetch(api_link)
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            return response.json();
-        })
-        .then(data => {
-            lochead.innerText = "Weather in " + location;
-            para.innerText = 
-                "Temperature : " + data.current.temp_c + "°C" +
-                "\n Condition : " + data.current.condition.text +
-                "\n Humidity : " + data.current.humidity + " percent" +
-                "\n Wind : " + data.current.wind_kph + " km/h";
-        })
-        .catch(error => {
-            console.log(error);
-            para.innerText = "Could not find weather data";
-        });
+        fetch(api_link)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.json();
+            })
+            .then(data => {
+                lochead.innerText = "Weather in " + location;
+                para.innerText = 
+                    "Temperature : " + data.current.temp_c + "°C" +
+                    "\n Condition : " + data.current.condition.text +
+                    "\n Humidity : " + data.current.humidity + " percent" +
+                    "\n Wind : " + data.current.wind_kph + " km/h";
+            })
+            .catch(error => {
+                console.log(error);
+                para.innerText = "Could not find weather data";
+            });
+        }   
 }
